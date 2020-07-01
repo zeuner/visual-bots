99375929ee127db5ffd6c3a4b46b0ebcca1274e6
Isidor Zeuner <minetest@quidecco.de>
2020-07-01 13:33:52 +0200

    increase elevator grab range

--- register_bot.lua
+++ register_bot.lua
@@ -153,9 +153,10 @@ local function move_bot(pos,direction)
         position_bot(pos,newpos)
     end
     if ppos then
-        if math.abs(ppos.x-pos.x)<1.1 and
-                math.abs(ppos.z-pos.z)<1.1 and
-                math.abs(ppos.y-pos.y)<2 and
+        local factor = 1.1
+        if math.abs(ppos.x-pos.x)<(1.1*factor) and
+                math.abs(ppos.z-pos.z)<(1.1*factor) and
+                math.abs(ppos.y-pos.y)<(2*factor) and
                 ppos.y>pos.y then
             player:setpos({x=newpos.x, y=newpos.y+1.1, z=newpos.z })
         end
